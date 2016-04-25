60455cba7f3e38524d7fad0506d56bce666f4ad1
lovelydreamer <christinamitchell08@gmail.com>
2016-04-25 16:47:45 -0500

    module 1-4

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+    <div>
+      <h1>React Router Tutorial</h1> 
+      <ul role="nav">
+        <li><Link to="/about" activeStyle={{ color: 'red' }}>About</Link></li>
+        <li><Link to="/repos" activeStyle={{ color: 'red' }}>Repos</Link></li>
+       </ul> 
+       {this.props.children}
+    </div>
+   )
   }
 })
